fab6214c0261815d0040ac4674f2abdafeb0341c
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2024-05-17 14:12:34 -0300

    Adição da exibição da previsão de temperatura a cada 3 horas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,37 +17,47 @@ const sensacao = document.querySelector('#sensacao span');
 const humidity = document.querySelector('#humidity span');
 const windy = document.querySelector('#windy-speed span');
 
-// const hour1Element = document.querySelector('#hour1');
-// const hour2Element = document.querySelector('#hour2');
-// const hour3Element = document.querySelector('#hour3');
-// const hour4Element = document.querySelector('#hour4');
+const horaTemperatura = document.querySelector('#time-hour');
 
-// const hourTemp1Element = document.querySelector('#day-temp1');
-// const hourTemp2Element = document.querySelector('#day-temp2');
-// const hourTemp3Element = document.querySelector('#day-temp3');
-// const hourTemp4Element = document.querySelector('#day-temp4');
+const temperaturaHour = document.querySelector('#temp-hour span');
+const temperaturaHour2 = document.querySelector('#temp-hour2 span');
+const temperaturaHour3 = document.querySelector('#temp-hour3 span');
+const temperaturaHour4 = document.querySelector('#temp-hour4 span');
+const temperaturaHour5 = document.querySelector('#temp-hour5 span');
+const temperaturaHour6 = document.querySelector('#temp-hour6 span');
+const temperaturaHour7 = document.querySelector('#temp-hour7 span');
 
-// const hourDescription1 = document.querySelector('#description1');
-// const hourDescription2 = document.querySelector('#description2');
-// const hourDescription3 = document.querySelector('#description3');
-// const hourDescription4 = document.querySelector('#description4');
+const tempHourHumidity = document.querySelector('#temp-hour-humidity span');
+const tempHourHumidity2 = document.querySelector('#temp-hour-humidity2 span');
+const tempHourHumidity3 = document.querySelector('#temp-hour-humidity3 span');
+const tempHourHumidity4 = document.querySelector('#temp-hour-humidity4 span');
+const tempHourHumidity5 = document.querySelector('#temp-hour-humidity5 span');
+const tempHourHumidity6 = document.querySelector('#temp-hour-humidity6 span');
+const tempHourHumidity7 = document.querySelector('#temp-hour-humidity7 span');
 
-// const hourIcon1Element = document.querySelector('#weather-icon1');
-// const hourIcon2Element = document.querySelector('#weather-icon2');
-// const hourIcon3Element = document.querySelector('#weather-icon3');
-// const hourIcon4Element = document.querySelector('#weather-icon4');
+const tempHourIcon = document.querySelector('#temp-hour-icon');
+const tempHourIcon2 = document.querySelector('#temp-hour-icon2');
+const tempHourIcon3 = document.querySelector('#temp-hour-icon3');
+const tempHourIcon4 = document.querySelector('#temp-hour-icon4');
+const tempHourIcon5 = document.querySelector('#temp-hour-icon5');
+const tempHourIcon6 = document.querySelector('#temp-hour-icon6');
+const tempHourIcon7 = document.querySelector('#temp-hour-icon7');
 
-// const hourHumidity1 = document.querySelector('#humidity1 span');
-// const hourHumidity2 = document.querySelector('#humidity2 span');
-// const hourHumidity3 = document.querySelector('#humidity3 span');
-// const hourHumidity4 = document.querySelector('#humidity4 span');
+const tempHourDescription = document.querySelector('#temp-hour-description');
+const tempHourDescription2 = document.querySelector('#temp-hour-description2');
+const tempHourDescription3 = document.querySelector('#temp-hour-description3');
+const tempHourDescription4 = document.querySelector('#temp-hour-description4');
+const tempHourDescription5 = document.querySelector('#temp-hour-description5');
+const tempHourDescription6 = document.querySelector('#temp-hour-description6');
+const tempHourDescription7 = document.querySelector('#temp-hour-description7');
 
-// const hourWindy1 = document.querySelector('#wind1 span');
-// const hourWindy2 = document.querySelector('#wind2 span');
-// const hourWindy3 = document.querySelector('#wind3 span');
-// const hourwindy4 = document.querySelector('#wind4 span');
-
-// const weatherContainer = document.querySelector("#weather-data");
+const timeHour = document.querySelector('#time-hour');
+const timeHour2 = document.querySelector('#time-hour2');
+const timeHour3 = document.querySelector('#time-hour3');
+const timeHour4 = document.querySelector('#time-hour4');
+const timeHour5 = document.querySelector('#time-hour5');
+const timeHour6 = document.querySelector('#time-hour6');
+const timeHour7 = document.querySelector('#time-hour7');
 
 //FUNÇÕES
 
@@ -76,7 +86,7 @@ const showWeatherData = async (city) => {
    humidity.innerText = `${data.main.humidity} %`;
    windy.innerText = `${parseInt(data.wind.speed)} Km/h`;
 
-   weatherContainer.classList.remove("hide");
+   //weatherContainer.classList.remove("hide");
 };
 
 //Pesquisando os dados das proximas 3 horas para os proximos 5 dias
@@ -87,47 +97,61 @@ const getForecastData = async (city) => {
    const res = await fetch(apiForecastUrl);
    const data = await res.json();
 
-   console.log(data);
-
    return data;
 }
 
 const showForecastData = async (city) => {
    const data = await getForecastData(city);
 
-   //cityElement.innerText = data.list.name;
 
-   // hour1Element.innerText = data.list[0].dt_txt;
-   // hour2Element.innerText = data.list[1].dt_txt;
-   // hour3Element.innerText = data.list[2].dt_txt;
-   // hour4Element.innerText = data.list[3].dt_txt;
+   temperaturaHour.innerText = parseInt(`${data.list[0].main.temp}&deg; C`);
+   temperaturaHour2.innerText = parseInt(data.list[1].main.temp);
+   temperaturaHour3.innerText = parseInt(data.list[2].main.temp);
+   temperaturaHour4.innerText = parseInt(data.list[3].main.temp);
+   temperaturaHour5.innerText = parseInt(data.list[4].main.temp);
+   temperaturaHour6.innerText = parseInt(data.list[5].main.temp);
+   temperaturaHour7.innerText = parseInt(data.list[6].main.temp);
 
-   // hourTemp1Element.innerText = parseInt(data.list[1].main.temp);
-   // hourTemp2Element.innerText = parseInt(data.list[2].main.temp);
-   // hourTemp3Element.innerText = parseInt(data.list[3].main.temp);
-   // hourTemp4Element.innerText = parseInt(data.list[4].main.temp);
+   tempHourHumidity.innerText = `${data.list[0].main.humidity} %`;
+   tempHourHumidity2.innerText = `${data.list[1].main.humidity} %`;
+   tempHourHumidity3.innerText = `${data.list[2].main.humidity} %`;
+   tempHourHumidity4.innerText = `${data.list[3].main.humidity} %`;
+   tempHourHumidity5.innerText = `${data.list[4].main.humidity} %`;
+   tempHourHumidity6.innerText = `${data.list[5].main.humidity} %`;
+   tempHourHumidity7.innerText = `${data.list[6].main.humidity} %`;
 
-   // hourDescription1.innerText = data.list[1].weather[0].description;
-   // hourDescription2.innerText = data.list[2].weather[0].description;
-   // hourDescription3.innerText = data.list[3].weather[0].description;
-   // hourDescription4.innerText = data.list[4].weather[0].description;
+   tempHourIcon.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`);
+   tempHourIcon2.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png`);
+   tempHourIcon3.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[2].weather[0].icon}.png`);
+   tempHourIcon4.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png`);
+   tempHourIcon5.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[4].weather[0].icon}.png`);
+   tempHourIcon6.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[5].weather[0].icon}.png`);
+   tempHourIcon7.setAttribute("src", `http://openweathermap.org/img/wn/${data.list[6].weather[0].icon}.png`);
 
-   // hourIcon1Element.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png`);
-   // hourIcon2Element.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[2].weather[0].icon}.png`);
-   // hourIcon3Element.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png`);
-   // hourIcon4Element.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[4].weather[0].icon}.png`);
+   tempHourDescription.innerText = data.list[0].weather[0].description;
+   tempHourDescription2.innerText = data.list[1].weather[0].description;
+   tempHourDescription3.innerText = data.list[2].weather[0].description;
+   tempHourDescription4.innerText = data.list[3].weather[0].description;
+   tempHourDescription5.innerText = data.list[4].weather[0].description;
+   tempHourDescription6.innerText = data.list[5].weather[0].description;
+   tempHourDescription7.innerText = data.list[6].weather[0].description;
 
-   // hourHumidity1.innerText = `${data.list[1].main.humidity}%`;
-   // hourHumidity2.innerText = `${data.list[2].main.humidity}%`;
-   // hourHumidity3.innerText = `${data.list[3].main.humidity}%`;
-   // hourHumidity4.innerText = `${data.list[4].main.humidity}%`;
-   
-   // hourWindy1.innerText = `${data.list[1].wind.speed}km/h`;
-   // hourWindy2.innerText = `${data.list[2].wind.speed}km/h`;
-   // hourWindy3.innerText = `${data.list[3].wind.speed}km/h`;
-   // hourwindy4.innerText = `${data.list[4].wind.speed}km/h`;
+   const horaformatada = data.list[0].dt_txt.slice(11, 16);
+   const horaformatada2 = data.list[1].dt_txt.slice(11, 16);
+   const horaformatada3 = data.list[2].dt_txt.slice(11, 16);
+   const horaformatada4 = data.list[3].dt_txt.slice(11, 16);
+   const horaformatada5 = data.list[4].dt_txt.slice(11, 16);
+   const horaformatada6 = data.list[5].dt_txt.slice(11, 16);
+   const horaformatada7 = data.list[6].dt_txt.slice(11, 16);
 
-   // weatherContainer.classList.remove("hide");
+   timeHour.innerText = horaformatada;
+   timeHour2.innerText = horaformatada2;
+   timeHour3.innerText = horaformatada3;
+   timeHour4.innerText = horaformatada4;
+   timeHour5.innerText = horaformatada5;
+   timeHour6.innerText = horaformatada6;
+   timeHour7.innerText = horaformatada7;
+
 };
 
 //EVENTOS
